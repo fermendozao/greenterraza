0cf2a00de4c6d3e99d4c63a44aa6676742af76d1
Istaak <fernandoistaak@gmail.com>
2018-05-14 22:03:00 -0500

    comp: Modify view of components

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -3,13 +3,15 @@ import React, {Component} from 'react';
 export class Hero extends Component {
   render() {
     return (
-      <div className="hero">
-        <h1 className="text-center">
+      <div className="hero-container">
+        <h1 className="text-center title">
           <small>Estamos contigo</small>
           en el día más feliz de tu vida
         </h1>
 
-        <button className="btn btn-default">Realizar Pedido</button>
+        <p className="text-center">
+          <button className="btn btn-default">Realizar Pedido</button>
+        </p>
       </div>
     );
   }
