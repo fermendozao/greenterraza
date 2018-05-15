4cdf57bc1fe3d8356fed91f6068cb589f020e0b7
Istaak <fernandoistaak@gmail.com>
2018-05-14 22:05:17 -0500

    comp: Modify Events component

--- src/app/components/Events.js
+++ src/app/components/Events.js
@@ -1,35 +1,94 @@
 import React, {Component} from 'react';
-import {Grid, Row, Col, Carousel} from 'react-bootstrap';
+import {Grid, Row, Col, Button, ButtonToolbar, ButtonGroup} from 'react-bootstrap';
+import {Card} from './Card/Card';
+
+const categories = [
+  'Arreglos de ocasión',
+  'Bautizos',
+  'Bodas',
+  'Eventos corporativos',
+  'Principal',
+  'XV años'
+];
 
 class Events extends Component {
-  componentDidMount() {
-    console.log(this.props);
-  }
 
   render() {
     return (
-      <Grid style={{marginTop: '100px'}}>
-        <h1>Eventos</h1>
+      <Grid style={{marginTop: '100px'}} className="events">
         <Row>
-          <Col xs={8}>
-            <Carousel>
-              <Carousel.Item>
-                <img width={900} height={500} alt="900x500" src="./assets/img/flor1.jpg"/>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img width={900} height={500} alt="900x500" src="./assets/img/flor2.jpg"/>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img width={900} height={500} alt="900x500" src="./assets/img/flor3.jpg"/>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img width={900} height={500} alt="900x500" src="./assets/img/flor4.jpg"/>
-              </Carousel.Item>
-            </Carousel>
-          </Col>
+          <Col><h1>Eventos</h1></Col>
+        </Row>
 
-          <Col xs={4}>
-            Es de que este sería el sidebar
+        <Row>
+          <Col xs={12} mdHidden lgHidden>
+            <ButtonToolbar>
+              <ButtonGroup>
+                {
+                  categories.map((categoria, index) => {
+                    return (
+                      <Button key={index} className="btn btn-default">{categoria}</Button>
+                    );
+                  })
+                }
+              </ButtonGroup>
+            </ButtonToolbar>
+          </Col>
+          <Col smHidden xsHidden md={3} >
+            <ButtonGroup vertical>
+              {
+                categories.map((categoria, index) => {
+                  return (
+                    <Button key={index} className="btn btn-default">{categoria}</Button>
+                  );
+                })
+              }
+            </ButtonGroup>
+          </Col>
+          <Col xs={12} md={9}>
+            <Row>
+              <Col xs={6} md={4}>
+                <Card
+                  image={'../assets/img/flor1.jpg'}
+                  modal
+                  />
+              </Col>
+
+              <Col xs={6} md={4}>
+                <Card
+                  image={'../assets/img/flor2.jpg'}
+                  modal
+                  />
+              </Col>
+
+              <Col xs={6} md={4}>
+                <Card
+                  image={'../assets/img/flor3.jpg'}
+                  modal
+                  />
+              </Col>
+
+              <Col xs={6} md={4}>
+                <Card
+                  image={'../assets/img/flor4.jpg'}
+                  modal
+                  />
+              </Col>
+
+              <Col xs={6} md={4}>
+                <Card
+                  image={'../assets/img/flor5.jpg'}
+                  modal
+                  />
+              </Col>
+
+              <Col xs={6} md={4}>
+                <Card
+                  image={'../assets/img/bouquet.jpg'}
+                  modal
+                  />
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Grid>
